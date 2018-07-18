141543be68c87c750db538c7ad211ac53b9027cc
RasmiKapuganti <mukularasmi@gmail.com>
2018-07-17 19:03:40 -0600

    scripts

---
+++ DialogButton.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity.Buttons;
+using HoloToolkit.UX.Dialog;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HoloToolkit.UX.Buttons
+{
+    /// <summary>
+    /// Handling click event and dismiss dialog
+    /// </summary>
+    public class DialogButton : MonoBehaviour
+    {
+        private Button buttonComponent;
+
+        private DialogShell parentDialog;
+
+        /// <summary>
+        /// A reference to the Dialog that this button is on.
+        /// </summary>
+        public DialogShell ParentDialog
+        {
+            get
+            {
+                return parentDialog;
+            }
+            set
+            {
+                parentDialog = value;
+            }
+        }
+
+        /// <summary>
+        /// The type description of the button
+        /// </summary>
+        public Dialog.DialogButtonType ButtonTypeEnum;
+
+        private void OnEnable()
+        {
+            buttonComponent = GetComponent<Button>();
+            buttonComponent.OnButtonClicked += OnButtonClicked;
+        }
+
+        private void OnDisable()
+        {
+            if (buttonComponent != null)
+            {
+                buttonComponent.OnButtonClicked -= OnButtonClicked;
+            }
+        }
+
+        /// <summary>
+        /// event handler that runs when button is clicked.
+        /// Dismisses the parent dialog.
+        /// </summary>
+        /// <param name="obj"></param>
+        public void OnButtonClicked(GameObject obj)
+        {
+            /// differernt scenes -
+            /// StartScene - the main starting scene with the dialog box
+            /// Scene1 - fires upon Exit button, exits the application
+            /// Scene2 - the object recognition starts, starts on button click of Continue button in StartScene
+
+            if(obj.name == "StartSceneContinue")
+            {
+               // Application.OpenURL("http://supportvideos.ext.hp.com/detail/video/4145094106001/connecting-an-hp-notebook-to-the-hp-ultra-slim-docking-station?autoStart=true&q=docking");
+                SceneManager.LoadScene("Scene2");
+            }
+            else if(obj.name == "StartSceneExit")
+            {
+                Application.Quit();
+            }
+            else if(obj.name == "ButtonTwoB")
+            {
+                CreateArrowObject arrow = new CreateArrowObject();
+
+            }
+
+
+            //if (parentDialog != null)
+            //{
+              //  parentDialog.Result.Result = ButtonTypeEnum;
+              // parentDialog.DismissDialog();
+            //}
+        }
+
+        /// <summary>
+        /// Setter Method to set the Text at the top of the Dialog.
+        /// </summary>
+        /// <param name="title"></param>
+        public void SetTitle(string title)
+        {
+            CompoundButtonText compoundButtonText = GetComponent<CompoundButtonText>();
+            if (compoundButtonText)
+            {
+                compoundButtonText.Text = title;
+            }
+        }
+    }
+}
---
+++ ObjectTrackableEvent.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace Vuforia
+{
+    /// Summary
+    /// A custom handler that implements the ITrackableEventHandler interface.
+
+    public class ObjectTrackableEvent : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        public GameObject TagOverlayControl;
+        private TrackableBehaviour mTrackableBehaviour;
+        private GameObject CurrentTextUI = null;
+
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        /// Summary
+        /// Implementation of the ITrackableEventHandler function called when the
+        /// tracking state changes.
+
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                OnTrackingFound();
+            }
+            else
+            {
+                OnTrackingLost();
+            }
+        }
+
+        private void OnTrackingFound()
+        {
+            if (CurrentTextUI != null)
+                return;
+
+            CurrentTextUI = (GameObject)Instantiate(this.TagOverlayControl);
+            CurrentTextUI.SetActive(true);
+
+            CurrentTextUI.transform.position = transform.position;
+            CurrentTextUI.transform.rotation = transform.rotation;
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+        private void OnTrackingLost()
+        {
+        }
+    }
+}
